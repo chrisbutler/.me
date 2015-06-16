74c292df0ff675ca9d5b2365d1f57e7f1c8c0ed2
Chris Butler <chris@platformthirteen.com>
2015-06-16 00:04:22 -0400

    add logo, social links, instagram & tumblr feeds, and latest tweet

---
+++ js/twitter.js
@@ -0,0 +1,35 @@
+// var cb = {
+//   "id": '19496635',
+//   "domId": 'tweets',
+//   "maxTweets": 1,
+//   "enableLinks": true
+// };
+
+var cb = {
+  "id": '610645340434239489',
+  "domId": 'tweets',
+  "maxTweets": 1,
+  "enableLinks": true,
+  "showUser": true,
+  "showTime": true,
+  "dateFunction": '',
+  "showRetweet": false,
+  "customCallback": handleTweets,
+  "showInteraction": false
+};
+
+function handleTweets(tweets){
+  console.log('tweets: ', tweets);
+    var x = tweets.length;
+    var n = 0;
+    var element = document.getElementById('tweets');
+    var html = '<ul>';
+    while(n < x) {
+      html += '<li>' + tweets[n] + '</li>';
+      n++;
+    }
+    html += '</ul>';
+    element.innerHTML = html;
+}
+
+twitterFetcher.fetch(cb);
